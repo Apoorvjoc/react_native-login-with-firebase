436ee20bbdd863283dcc0024697db5beced58966
Apoorv <hlwapoorv@gmail.com>
2021-12-10 17:29:50 +0530

    added header inside axios

--- screens/HandlingRapidAPI.js
+++ screens/HandlingRapidAPI.js
@@ -5,33 +5,58 @@ import axios from 'axios';
 const HandlingRapidAPI = () => {
 
     const [loading, setLoading] = useState(true)
+    const [err, setErr] = useState('')
     const [data, setData] = useState([])
 
     useEffect(()=>{
+    //     let lat = 28.6280;
+    //     let lng = 77.3649;
+    //    var options = {
+    //         method: 'GET',
+    //         url:`https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=${lat}&longitude=${lng}`,
+    //         // url: `https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng`,
+    //         // params: {
+    //         //     latitude: '28.6280',
+    //         //     longitude: '77.3649',
+    //         // },
+    //         headers: {
+    //             'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+    //             'x-rapidapi-key': '8dedc09fa7msh378ca98ae765a1dp1cc83cjsn58584d832b75'
+    //         }
+    //         };
+
+
+
+            // axios.request(options).then(function (response) {
+            //     // console.log(response.data.data[0].name +"23");
+            //     // console.log(response.data);
+            //     setData(response.data.data)
+            // }).catch(function (error) {
+            //     console.error(error);
+            // });
+            apiCall();
+    },[])
+
+    let apiCall = async()=>{
         let lat = 28.6280;
         let lng = 77.3649;
-       var options = {
-            method: 'GET',
-            url:`https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=${lat}&longitude=${lng}`,
-            // url: `https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng`,
-            // params: {
-            //     latitude: '28.6280',
-            //     longitude: '77.3649',
-            // },
-            headers: {
-                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-                'x-rapidapi-key': '8dedc09fa7msh378ca98ae765a1dp1cc83cjsn58584d832b75'
-            }
-            };
-
-            axios.request(options).then(function (response) {
-                // console.log(response.data.data[0].name +"23");
-                // console.log(response.data);
-                setData(response.data.data)
-            }).catch(function (error) {
-                console.error(error);
-            });
-    },[])
+        try{
+            let response = await axios.get(
+                `https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=${lat}&longitude=${lng}`,
+                {
+                headers: {
+                    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+                    'x-rapidapi-key': '8dedc09fa7msh378ca98ae765a1dp1cc83cjsn58584d832b75'
+                    }
+                }
+            )
+            setLoading(false);
+            setData(response.data.data);
+        }catch(err){
+            setErr(err.message)
+            setLoading(false);
+        }
+    }
 
     return (
         <View  style={styles.container}>
