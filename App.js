1c363e7da0c20c9bbb79039a1fea6a17be28bd2e
Apoorv <hlwapoorv@gmail.com>
2021-12-10 13:29:56 +0530

    handling api calls with axios

--- App.js
+++ App.js
@@ -5,17 +5,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import loginScreen from './screens/loginScreen';
 import HomeScreen from './screens/HomeScreen';
+import ApiCall from './screens/ApiCall';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-         <Stack.Screen options={{headerShown:false}} name="login" component={loginScreen} />
-         <Stack.Screen name="home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //      <Stack.Screen options={{headerShown:false}} name="login" component={loginScreen} />
+    //      <Stack.Screen name="home" component={HomeScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <ApiCall/>
   );
 }
 
