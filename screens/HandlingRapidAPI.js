63b8bc570234825fc41ad54c8764c8325762e5a2
Apoorv <hlwapoorv@gmail.com>
2021-12-11 09:22:36 +0530

    handled api call with axios

--- screens/HandlingRapidAPI.js
+++ screens/HandlingRapidAPI.js
@@ -1,5 +1,5 @@
 import React, { useState , useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View , TextInput } from 'react-native'
 import axios from 'axios';
 
 const HandlingRapidAPI = () => {
@@ -7,43 +7,21 @@ const HandlingRapidAPI = () => {
     const [loading, setLoading] = useState(true)
     const [err, setErr] = useState('')
     const [data, setData] = useState([])
+    const [lat, setLat] = useState(28.6280)
+    const [lng, setLng] = useState(77.3649)
+
 
     useEffect(()=>{
-    //     let lat = 28.6280;
-    //     let lng = 77.3649;
-    //    var options = {
-    //         method: 'GET',
-    //         url:`https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=${lat}&longitude=${lng}`,
-    //         // url: `https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng`,
-    //         // params: {
-    //         //     latitude: '28.6280',
-    //         //     longitude: '77.3649',
-    //         // },
-    //         headers: {
-    //             'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-    //             'x-rapidapi-key': '8dedc09fa7msh378ca98ae765a1dp1cc83cjsn58584d832b75'
-    //         }
-    //         };
+            restaurantsApiCall();
+    } , [lat , lng])
 
-
-
-            // axios.request(options).then(function (response) {
-            //     // console.log(response.data.data[0].name +"23");
-            //     // console.log(response.data);
-            //     setData(response.data.data)
-            // }).catch(function (error) {
-            //     console.error(error);
-            // });
-            apiCall();
-    },[])
-
-    let apiCall = async()=>{
-        let lat = 28.6280;
-        let lng = 77.3649;
+    let restaurantsApiCall = async()=>{
+        // let lat = 28.6280;
+        // let lng = 77.3649;
+        console.log("lat:" , lat);
+        console.log("lng:" , lng);
         try{
-            let response = await axios.get(
-                `https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=${lat}&longitude=${lng}`,
-                {
+            let response = await axios.get(`https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=${lat}&longitude=${lng}`,{
                 headers: {
                     'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
                     'x-rapidapi-key': '8dedc09fa7msh378ca98ae765a1dp1cc83cjsn58584d832b75'
@@ -52,6 +30,7 @@ const HandlingRapidAPI = () => {
             )
             setLoading(false);
             setData(response.data.data);
+            // console.log(response.data.data);
         }catch(err){
             setErr(err.message)
             setLoading(false);
@@ -60,9 +39,19 @@ const HandlingRapidAPI = () => {
 
     return (
         <View  style={styles.container}>
+        <TextInput 
+         placeholder = "enter latitude"
+         onChangeText={text =>setLat(text)}
+        />
+        <TextInput 
+         placeholder = "enter longitude"
+         onChangeText={text =>setLng(text)}
+        />
         {
-            data.map((item , idx)=>(
-                <Text key={idx}>{item.name}{item.location_id}</Text>
+            data.slice(0,10).map((item , idx)=>(
+                <Text key={idx}>{item.name}
+                {/*item.location_id*/}
+                </Text>
             ))
         }
         </View>
@@ -77,5 +66,4 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-  },
 })
